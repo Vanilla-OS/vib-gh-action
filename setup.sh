b818be7bd5224b38c80ba2921b8459c994d6afbb
Mateus Melchiades <mateusbmelchiades@gmail.com>
2024-06-26 08:45:44 -0300

    chore: bump Vib to v0.7.2

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VIB_VERSION="0.7.1"
+VIB_VERSION="0.7.2"
 PLUGINS_ARG="${1:-}"
 
 wget "https://github.com/Vanilla-OS/Vib/releases/download/v$VIB_VERSION/plugins.tar.xz"
@@ -8,24 +8,24 @@ tar -xf plugins.tar.xz
 mv build/plugins plugins
 
 if [ -z "$PLUGINS_ARG" ]; then
-    echo "No plugins specified, using static Vib binary"
+	echo "No plugins specified, using static Vib binary"
 else
-    echo "Plugins specified, downloading plugin assets..."
+	echo "Plugins specified, downloading plugin assets..."
 
-    IFS=',' read -ra PLUGIN_LIST <<< "$PLUGINS_ARG"
-    for PLUGIN in "${PLUGIN_LIST[@]}"; do
-        REPO=$(echo "$PLUGIN" | awk -F':' '{print $1}')
-        TAG=$(echo "$PLUGIN" | awk -F':' '{print $2}')
-        
-        echo "Downloading assets for $REPO..."
-        
-        ASSETS_URL="https://api.github.com/repos/$REPO/releases/tags/$TAG"
-        ASSET_URLS=$(curl -s "$ASSETS_URL" | grep -o -E 'https://github.com/[^"]+\.so')
+	IFS=',' read -ra PLUGIN_LIST <<<"$PLUGINS_ARG"
+	for PLUGIN in "${PLUGIN_LIST[@]}"; do
+		REPO=$(echo "$PLUGIN" | awk -F':' '{print $1}')
+		TAG=$(echo "$PLUGIN" | awk -F':' '{print $2}')
 
-        for ASSET_URL in $ASSET_URLS; do
-            wget -P plugins/ "$ASSET_URL"
-        done
-    done
+		echo "Downloading assets for $REPO..."
+
+		ASSETS_URL="https://api.github.com/repos/$REPO/releases/tags/$TAG"
+		ASSET_URLS=$(curl -s "$ASSETS_URL" | grep -o -E 'https://github.com/[^"]+\.so')
+
+		for ASSET_URL in $ASSET_URLS; do
+			wget -P plugins/ "$ASSET_URL"
+		done
+	done
 fi
 
 wget "https://github.com/Vanilla-OS/Vib/releases/download/v$VIB_VERSION/vib"
